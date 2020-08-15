02c5c86383c9207dd5bcf55f73203616946b18cc
Md. Alamgir Hossain <alamgirakash2000@gmail.com>
2020-08-15 15:19:18 +0600

    Updates

---
+++ precache-manifest.f617f677f7d352f123d93c5a71e50131.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5cef8034e8aa21d71e50c2ecf4a38fe",
+    "url": "/newchatapp/index.html"
+  },
+  {
+    "revision": "20fe09b28f6ffe7a3027",
+    "url": "/newchatapp/static/css/main.d3b77593.chunk.css"
+  },
+  {
+    "revision": "69db36a1b89065e9aab7",
+    "url": "/newchatapp/static/js/2.741257f5.chunk.js"
+  },
+  {
+    "revision": "3ceb8c17f560e6682fd3523301e674b3",
+    "url": "/newchatapp/static/js/2.741257f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20fe09b28f6ffe7a3027",
+    "url": "/newchatapp/static/js/main.6f033127.chunk.js"
+  },
+  {
+    "revision": "8810ed5b8d6b3b5f5a65",
+    "url": "/newchatapp/static/js/runtime-main.9e96ce96.js"
+  }
+]);
